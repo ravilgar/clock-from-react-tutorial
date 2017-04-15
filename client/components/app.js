729c59d1dc66ded00223a904409591241f999d68
ravilgar <ravil_g@mail.ru>
2017-04-15 19:11:44 +0300

    beautify code

--- client/components/app.js
+++ client/components/app.js
@@ -29,9 +29,15 @@ export default class App extends React.Component {
     }
 
     render() {
-        return ( < div >
-            < h1 > Hello, world! < /h1> < h2 > It is { this.state.date.toLocaleTimeString() }. < /h
-            2 > < /div>
+        return ( 
+            <div>
+                <h1>
+                    Hello, world! 
+                 </h1> 
+                 < h2 > 
+                    It is { this.state.date.toLocaleTimeString() }. 
+                 </h2> 
+            < /div>
         );
     }
 }
